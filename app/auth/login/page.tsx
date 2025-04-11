c6ee84d3006a2399fdfb9bbc9c72c26a752dc5b0
enhuili <lexi05732@gmail.com>
2025-04-11 17:11:31 -0400

    Commented out forgot password link

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -71,12 +71,12 @@ export default function LoginPage() {
             <div className="space-y-2">
               <div className="flex items-center justify-between">
                 <Label htmlFor="password">Password</Label>
-                <Link
+                {/* <Link
                   href="/auth/forgot-password"
                   className="text-sm text-blue-600 hover:underline"
                 >
                   Forgot password?
-                </Link>
+                </Link> */}
               </div>
               <Input
                 id="password"
